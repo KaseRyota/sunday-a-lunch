bd04d6fe7926126e5074e5a9184ab4fbc465d5ee
kaseryota <football.striker.60@gmail.com>
2018-11-15 02:14:28 +0000

    cloudinary done

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ActiveRecord::Base
+    attr_accessor :photo
     mount_uploader :photo, PhotoUploader
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -12,6 +12,23 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+  include Cloudinary::CarrierWave
+
+  process :convert => 'jpg'
+  process :tags => ['photo']
+
+  version :standard do
+    process :resize_to_fill => [100, 150, :north]
+  end
+
+  version :thumbnail do
+    process :resize_to_fit => [50, 50]
+  end
+
+  def public_id
+    return model.id
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
